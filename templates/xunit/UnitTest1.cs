c93c09ae62dd5f1b4dbfe81de425a6c72f2884be
Fatih Tatoğlu <fatihtatoglu@gmail.com>
2022-07-28 09:03:19 +0300

    add xunit template

---
+++ templates/xunit/UnitTest1.cs
@@ -0,0 +1,20 @@
+namespace Company.TestProject1
+{
+    using System;
+
+    using Xunit;
+
+    public sealed class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            
+        }
+    }
+}
